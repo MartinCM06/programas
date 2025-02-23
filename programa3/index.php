a5421dc36200d8609a0939fab1a72eba05a82139
MartinCM06 <pelonmartincito@gmail.com>
2025-02-23 17:04:06 -0600

    Add files via upload

---
+++ programa3/index.php
@@ -0,0 +1,19 @@
+<?php
+$num1 = 10;
+$num2 = 5;
+
+
+$suma = $num1 + $num2;
+
+
+$resta = $num1 - $num2;
+
+
+$multiplicacion = $num1 * $num2;
+$division = $num1 / $num2;
+
+echo "Suma:  $suma <br>";
+echo "Resta:  $resta <br>";
+echo "Multiplicación: $multiplicacion <br>";
+echo "División: $division <br>";
+?>
